8d09586e7a7a148398fb5cff752d0276ec9ff53c
Anirudh Ramnath <anirudh@Anirudhs-MacBook-Pro.local>
2015-09-19 21:57:27 -0500

    some changes

--- parsefile.py
+++ parsefile.py
@@ -10,12 +10,12 @@ def parse_input_file(filename):
         for line in f:
             data = line.strip()[1:-1]
             m = re.findall('(?:\{([\d\s,]+)(?=\}))+', data)
-            
+
             if len(m) > 0:
                 row = []
                 print m
                 for item in m:
-                    transaction = [int(x.strip()) for x in item.split(',')]
+                    transaction = [str(x.strip()) for x in item.split(',')]
                     row.append(transaction)
 
                 result.append(row)
@@ -23,17 +23,24 @@ def parse_input_file(filename):
         return result
 
 
-""" Function to parse parameter file """
+""" Function to parse parameter file
+    Returns a tuple (dictionary of minsups, sdc) """
 def parse_param_file(filename):
     result = {}
+    sdc = 0.0
 
     with open(filename, 'r') as f:
         for line in f:
+
+            if line.startswith('SDC'):
+                sdc = float(line.split('=')[1].strip());
+
             data = line.strip()
             m = re.findall('(?:MIS\((\d+)\)\s+=\s+(.+))', data)
 
             if len(m) > 0:
-                result[m[0][0]] = m[0][1]
-                print m
+                result[m[0][0]] = float(m[0][1])
 
-        return result
+        return result, sdc
+
+parse_input_file('data.txt')
